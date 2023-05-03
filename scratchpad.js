f0f69ac8d8347347c722dee34b4f42c7336e65fc
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-03 15:08:10 -0700

    added working scratchpad for lessons

---
+++ scratchpad.js
@@ -0,0 +1,26 @@
+// GETTING ASCII VALUES
+// console.log("B".charCodeAt(0));
+
+// CALCULATOR EXERCISE
+// var firstNumber = prompt( "Please enter first number" );
+// var secondNumber = prompt( "Please enter second number" );
+// var sum = Number(firstNumber) + Number(secondNumber)
+// alert( "The sum is " + sum );
+
+// KEYLESS CARR EXERCISE
+// var age = Number(prompt("Please enter your age"));
+// if (age < 18) {
+// 	alert("Sorry, you are too young to drive this car.  Powering off");
+// }
+// if (age === 18) {
+// 	alert("Congratulations on your first year of driving. Enjoy the ride!");
+// } else {
+// 	alert("Powering On. Enjoy the ride!");
+// }
+
+// FUNCTION EXPRESSION
+var sayBye = function () {
+	console.log("Bye");
+};
+
+sayBye();
