d30e959f0334fb6364951e92cec337d99223b6ed
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-04 09:13:24 -0700

    keyless car #2 exercises solved

---
+++ keylessCar2.js
@@ -0,0 +1,47 @@
+// FUNCTION DECLARATION
+function checkDriverAge ()
+{
+    var age = prompt("What is your age?");
+    if (age < 18) {
+        alert("Sorry, you are too young to drive this car.  Powering off");
+    } else if (age === 18) {
+        alert(
+            "Congratulations on your first year of driving.  Enjoy the ride!"
+        );
+    } else if (age > 18) {
+        alert("Powering On.  Enjoy the ride!");
+    }
+}
+
+
+//FUNCTION EXPRESSION
+var checkDriverAge2 = function ()
+{
+    var age = prompt( "What is your age?" );
+    if ( age < 18 ) {
+        alert( "Sorry, you are too young to drive this car.  Powering off" );
+    } else if ( age === 18 ) {
+        alert(
+            "Congratulations on your first year of driving.  Enjoy the ride!"
+        );
+    } else if ( age > 18 ) {
+        alert( "Powering On.  Enjoy the ride!" );
+    }
+};
+
+
+function checkDriverAge3 (age) {
+    if (age < 18) {
+        alert("Sorry, you are too young to drive this car.  Powering off");
+    } else if (age === 18) {
+        alert(
+            "Congratulations on your first year of driving.  Enjoy the ride!"
+        );
+    } else if (age > 18) {
+        alert("Powering On.  Enjoy the ride!");
+    }// body
+}
+
+checkDriverAge();
+checkDriverAge2;
+checkDriverAge3(18);
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function multiply(a, b) {
-	if (a > 10 || b > 10) {
-		return "that's too hard";
-	} else {
-		return a * b;
-	}
+    if (a > 10 || b > 10) {
+        return "that's too hard";
+    }
+    return a * b;
+
 }
 
 alert(multiply(5, 40));
