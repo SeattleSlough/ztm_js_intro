5da73c1a2ecf3143d6b79a0260de9a91e24386ae
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-05 11:45:35 -0700

    completed OBJ lesson

--- objectsLesson.js
+++ objectsLesson.js
@@ -3,7 +3,12 @@ var user = {
     age: 34,
     hobby: "Soccer",
     isMarried: false,
+    shout: function () {
+        console.log("AHHHHHHHHH");
+    }
 };
+
+// A function within an object is called a METHOD
 
 user.favoriteFood = "spinach";
 
@@ -13,9 +18,32 @@ user.isMarried = true;
 
 console.log(user);
 
-var list = ["abrakadabra", "shazaam", "presto"]
+var list = ["abrakadabra", "shazaam", "presto"];
 
 user.spells = list;
 
 console.log(user);
+
+var list2 = [
+    {
+        username: "andy",
+        password: "secret",
+    },
+    {
+        username: "jess",
+        password: "123"
+    }
+];
+
+console.log(list2);
+
+var emptyObj = {};
+console.log(emptyObj); // {}
+emptyObj.name = "Andy"; // {name: "Andy"}
+
+var nullObj = null;
+console.log(nullObj); //null
+nullObj.name = "Andy"; // Uncaught TypeError as can add to a null Obj
+
+
 
