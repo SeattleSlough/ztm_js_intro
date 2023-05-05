dd6dee3e22fbfc28c86fdd61158cf5480e47d53e
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-05 14:43:03 -0700

    initial commit for last exercise (on script.js)

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-function multiply(a, b) {
-    if (a > 10 || b > 10) {
-        return "that's too hard";
-    }
-    return a * b;
+// function multiply(a, b) {
+//     if (a > 10 || b > 10) {
+//         return "that's too hard";
+//     }
+//     return a * b;
 
+// }
+
+// alert(multiply(5, 40));
+
+
+var obj = {
+    username: "Michael",
+    password: "12345",
+};
+
+var database = [ obj ];
+
+var obj1 = {
+    username: "Barb",
+    timeline: "So tired from all that learning!",
+};
+var obj2 = {
+    username: "Chris",
+    timeline: "Javascript is sooooo cool!",
+};
+var obj3 = {
+    username: "Jackie",
+    timeline: "Javascript is preeetyy cool!",
+};
+
+var newsfeed = [ obj1, obj2, obj3 ];
+
+var userNamePrompt = prompt( "What's your username?" )
+var passwordPrompt = prompt( "What's your password?" )
+
+function signIn( user, pass ) {
+    if( user === database[ 0 ].username && pass === database[ 0 ].password ) {
+        console.log( newsfeed );
+    } else {
+        alert( "Sorry, wrong username or password" )
+    }
 }
 
-alert(multiply(5, 40));
+signIn( userNamePrompt, passwordPrompt );
