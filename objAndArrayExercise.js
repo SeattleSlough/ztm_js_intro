49e09074161a751f2bc0897ed22779c48917bea8
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-05 12:24:47 -0700

    exercise validated

---
+++ objAndArrayExercise.js
@@ -0,0 +1,24 @@
+var obj = {
+    username: "Michael",
+    password: "12345",
+};
+
+var database = [ obj ];
+
+var obj1 = {
+    username: "Barb",
+    timeline: "So tired from all that learning!",
+};
+var obj2 = {
+    username: "Chris",
+    timeline: "Javascript is sooooo cool!",
+};
+var obj3 = {
+    username: "Jackie",
+    timeline: "Javascript is preeetyy cool!",
+};
+
+var newsfeed = [ obj1, obj2, obj3 ];
+
+
+console.log( database, newsfeed[ 0 ] )
