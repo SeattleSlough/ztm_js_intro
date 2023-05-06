606cbaae6434618ed9df65125459cccb7d0a6c3f
Michael J Maggs <seattleslew@runawayserver.com>
2023-05-06 14:39:29 -0700

    fb w/loops exercise

--- script.js
+++ script.js
@@ -9,12 +9,20 @@
 // alert(multiply(5, 40));
 
 
-var obj = {
-    username: "Michael",
-    password: "12345",
-};
-
-var database = [ obj ];
+var database = [
+    {
+        username: "andrei",
+        password: "supersecret",
+    },
+    {
+        username: "sally",
+        password: "123",
+    },
+    {
+        username: "ingrid",
+        password: "777",
+    },
+];
 
 var obj1 = {
     username: "Barb",
@@ -31,15 +39,17 @@ var obj3 = {
 
 var newsfeed = [ obj1, obj2, obj3 ];
 
-var userNamePrompt = prompt( "What's your username?" )
-var passwordPrompt = prompt( "What's your password?" )
-
 function signIn( user, pass ) {
-    if( user === database[ 0 ].username && pass === database[ 0 ].password ) {
-        console.log( newsfeed );
-    } else {
-        alert( "Sorry, wrong username or password" )
+    for( let i = 0; i < database.length; i++ ) {
+        if( database[ i ].username === user && database[ i ].password === pass ) {
+            console.log( newsfeed )
+        } else {
+            alert( "Sorry, wrong username or password" );
+        }
     }
 }
+
+var userNamePrompt = prompt( "What's your username?" );
+var passwordPrompt = prompt( "What's your password?" );
 
 signIn( userNamePrompt, passwordPrompt );
